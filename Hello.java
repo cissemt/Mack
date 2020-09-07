482aee3b5fb8d23cad17a2204a766399522b5275
cissemt <62968349+cissemt@users.noreply.github.com>
2020-09-06 22:01:45 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("<Macky Cisse>");
+		System.out.println("<Lab 01>");
+		System.out.println("<First lab of the semester>");
+		System.out.println("<09062020>");
+		System.out.println("<255 004>");
+		System.out.println();
+		}
+}
